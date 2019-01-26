3b4c9bdf252adfc9f50f28f646cf21d2ea90b9ee
Yun Hsiao Wu <yunhsiaow@gmail.com>
2019-01-26 08:40:41 +0800

    bloom

---
+++ assets/renderTextures.js
@@ -0,0 +1,59 @@
+cc.macro.ENABLE_WEBGL_ANTIALIAS = true;
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        sceneCamera: cc.Camera,
+        camBloomCut: cc.Camera,
+        camBloomBlurV: cc.Camera,
+        camBloomBlurH: cc.Camera,
+        spBloomCut: cc.Sprite,
+        spBloomBlurV: cc.Sprite,
+        spBloomBlurH: cc.Sprite,
+        spBloomBlend: cc.Sprite,
+    },
+
+    start () {
+        const gl = cc.game._renderContext;
+        this.spBloomCut.node.width = cc.visibleRect.width;
+        this.spBloomCut.node.height = cc.visibleRect.height * 0.9;
+        this.spBloomBlurV.node.width = cc.visibleRect.width;
+        this.spBloomBlurV.node.height = cc.visibleRect.height * 0.9;
+        this.spBloomBlurH.node.width = cc.visibleRect.width;
+        this.spBloomBlurH.node.height = cc.visibleRect.height * 0.9;
+        this.spBloomBlend.node.width = cc.visibleRect.width;
+        this.spBloomBlend.node.height = cc.visibleRect.height * 0.9;
+
+        const t1 = new cc.RenderTexture();
+        t1.initWithSize(cc.visibleRect.width, cc.visibleRect.height, gl.DEPTH_STENCIL);
+        const t2 = new cc.RenderTexture();
+        t2.initWithSize(cc.visibleRect.width, cc.visibleRect.height, gl.DEPTH_STENCIL);
+        const t3 = new cc.RenderTexture();
+        t3.initWithSize(cc.visibleRect.width, cc.visibleRect.height, gl.DEPTH_STENCIL);
+        const t4 = new cc.RenderTexture();
+        t4.initWithSize(cc.visibleRect.width, cc.visibleRect.height, gl.DEPTH_STENCIL);
+        const tex_offset = cc.v2(1 / cc.visibleRect.width, 1 / cc.visibleRect.height);
+
+        this.sceneCamera.targetTexture = t1;
+        const m1 = this.spBloomCut.getMaterial(0);
+        m1.setProperty('texture', t1.getImpl());
+
+        this.camBloomCut.targetTexture = t2;
+        const m2 = this.spBloomBlurV.getMaterial(0);
+        m2.setProperty('texture', t2.getImpl());
+        m2.setProperty('params', cc.quat(tex_offset.x, tex_offset.y, 1, 0.227027));
+        m2.setProperty('weights', cc.quat(0.1945946, 0.1216216, 0.054054, 0.016216));
+
+        this.camBloomBlurV.targetTexture = t3;
+        const m3 = this.spBloomBlurH.getMaterial(0);
+        m3.setProperty('texture', t3.getImpl());
+        m3.setProperty('params', cc.quat(tex_offset.x, tex_offset.y, 0, 0.227027));
+        m3.setProperty('weights', cc.quat(0.1945946, 0.1216216, 0.054054, 0.016216));
+
+        this.camBloomBlurH.targetTexture = t4;
+        const m4 = this.spBloomBlend.getMaterial(0);
+        m4.setProperty('origin', t1.getImpl());
+        m4.setProperty('blurred', t4.getImpl());
+    },
+});
