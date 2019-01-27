9d2e44bad26c9a0c7d6c2de597731159b13a9267
pandamicro <huabin.ling@cocos2d-x.org>
2019-01-27 10:43:32 +0800

    Add complete colliders

--- assets/scripts/physics/physics-settings.js
+++ assets/scripts/physics/physics-settings.js
@@ -2,11 +2,9 @@ cc.game.on(cc.game.EVENT_ENGINE_INITED, () => {
     let physicsManager = cc.director.getPhysicsManager();
     physicsManager.enabled = true;
     physicsManager.gravity = cc.v2();
-    
-    physicsManager.debugDrawFlags = 
-        // 0;
-        cc.PhysicsManager.DrawBits.e_aabbBit |
+
+    cc.director.getPhysicsManager().debugDrawFlags = 
+        (cc.PhysicsManager.DrawBits.e_aabbBit |
         cc.PhysicsManager.DrawBits.e_jointBit |
-        cc.PhysicsManager.DrawBits.e_shapeBit
-        ;
+        cc.PhysicsManager.DrawBits.e_shapeBit);
 });
--- assets/scripts/physics/physics-world.js
+++ assets/scripts/physics/physics-world.js
@@ -3,10 +3,19 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
+        debug: true
     },
 
     // use this for initialization
     onLoad: function () {
+        if (!CC_EDITOR) {
+            cc.director.getPhysicsManager().debugDrawFlags = 
+                this.debug ? 
+                (cc.PhysicsManager.DrawBits.e_aabbBit |
+                cc.PhysicsManager.DrawBits.e_jointBit |
+                cc.PhysicsManager.DrawBits.e_shapeBit) : 
+                0;
+        }
         cc.director.getPhysicsManager()._debugDrawer.node.group = '3d';
     }
 });
