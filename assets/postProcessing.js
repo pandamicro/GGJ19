98978da5afd646f244c779b63ebb7b149fbaa731
Yun Hsiao Wu <yunhsiaow@gmail.com>
2019-02-09 23:23:41 +0800

    light shaft

--- assets/postProcessing.js
+++ assets/postProcessing.js
@@ -20,24 +20,26 @@ cc.Class({
 
     onLoad () {
         const gl = cc.game._renderContext;
-        this.spBloomCut.node.width = cc.visibleRect.width;
-        this.spBloomCut.node.height = cc.visibleRect.height * 0.9; // HACK: amend for weird stretching
-        this.spBloomBlurV.node.width = cc.visibleRect.width;
-        this.spBloomBlurV.node.height = cc.visibleRect.height * 0.9;
-        this.spBloomBlurH.node.width = cc.visibleRect.width;
-        this.spBloomBlurH.node.height = cc.visibleRect.height * 0.9;
-        this.spBloomBlend.node.width = cc.visibleRect.width;
-        this.spBloomBlend.node.height = cc.visibleRect.height;
+        const w = cc.visibleRect.width, h = cc.visibleRect.height;
+        const sw = cc.visibleRect.width * 0.5, sh = cc.visibleRect.height * 0.5;
+
+        this.spBloomCut.node.width = sw;
+        this.spBloomCut.node.height = sh * 0.9; // HACK: amend for weird stretching
+        this.spBloomBlurV.node.width = sw;
+        this.spBloomBlurV.node.height = sh * 0.9;
+        this.spBloomBlurH.node.width = sw;
+        this.spBloomBlurH.node.height = sh * 0.9;
+        this.spBloomBlend.node.width = w;
+        this.spBloomBlend.node.height = h;
 
         const t1 = new cc.RenderTexture();
-        t1.initWithSize(cc.visibleRect.width, cc.visibleRect.height, gl.DEPTH_STENCIL);
+        t1.initWithSize(w, h, gl.DEPTH_STENCIL);
         const t2 = new cc.RenderTexture();
-        t2.initWithSize(cc.visibleRect.width, cc.visibleRect.height, gl.DEPTH_STENCIL);
+        t2.initWithSize(sw, sh, gl.DEPTH_STENCIL);
         const t3 = new cc.RenderTexture();
-        t3.initWithSize(cc.visibleRect.width, cc.visibleRect.height, gl.DEPTH_STENCIL);
-        console.log(cc.visibleRect.width, cc.visibleRect.height);
-        const tex_offset = cc.v2(1 / cc.visibleRect.width, 1 / cc.visibleRect.height);
-        const resolution = cc.v3(cc.visibleRect.width, cc.visibleRect.height, cc.visibleRect.width / cc.visibleRect.height);
+        t3.initWithSize(sw, sh, gl.DEPTH_STENCIL);
+        const tex_offset = cc.v2(1 / sw, 1 / sh);
+        const resolution = cc.v3(sw, sh, sw / sh);
         const kernelHead = 0.227027;
         const kernel = cc.quat(0.1945946, 0.1216216, 0.054054, 0.016216);
 
@@ -66,10 +68,12 @@ cc.Class({
         this.step = 0;
         this.targetTime = -1;
         this.currentValue = 0.5;
+        this.m4 = m4;
     },
 
     update () {
         const t = cc.director._totalFrames;
+        this.m4.setProperty('u_time', t / 30);
         if (t > this.targetTime) return;
         const value = this.currentValue + this.step;
         this.vigMat.setProperty('vignette', value);
